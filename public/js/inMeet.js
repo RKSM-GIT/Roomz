304b3f06d580620eac6d90df27e6809a832d8b50
RKSM-GIT <m.r.k.s@iitg.ac.in>
2022-07-12 11:48:44 +0530

    Finishing Touches

--- public/js/inMeet.js
+++ public/js/inMeet.js
@@ -59,7 +59,7 @@ function connectToNewUser(userId, stream) {
   let call = myPeer.call(userId, stream);
   const video = document.createElement("video");
   call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream, userID);
+    addVideoStream(video, userVideoStream);
   });
   call.on("close", () => {
     //console.log('closing');
